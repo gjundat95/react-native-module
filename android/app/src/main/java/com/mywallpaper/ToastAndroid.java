9d5d66f3e5d5ae5bff303a95ba262ee397f55635
Ngô Doãn Tình <gjundat95@gmail.com>
2017-08-09 23:51:09 +0700

    git create module

---
+++ android/app/src/main/java/com/mywallpaper/ToastAndroid.java
@@ -0,0 +1,43 @@
+package com.mywallpaper;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by tinhngo on 09/08/2017.
+ */
+
+public class ToastAndroid extends ReactContextBaseJavaModule {
+
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
+    public ToastAndroid(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ToastModule";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
+    }
+
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
+    }
+}
